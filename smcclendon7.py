b968a09bcd00444fefd25e8ff2adbc92d5080801
souljuhs <c7rtier@gmail.com>
2024-10-23 01:33:07 +0000

    lab 7

---
+++ smcclendon7.py
@@ -0,0 +1,19 @@
+
+x = 4
+y = 5.6
+z = "Maine"
+
+if x > 3:
+    print("x is greater than 3")
+else:
+    print("x is not greater than 3")
+
+if y > 5.0:
+    print("y is greater than 5")
+else:
+    print("y is not greater than ")
+
+if z == "California":
+    print("Variable z is California")
+else:
+    print("Variable z is not California")
